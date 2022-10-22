73f2bc2cf8a7a2640215585203b5e82cd8720306
corbatsh <bortlikkamil@gmail.com>
2022-10-22 18:37:03 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+/*
+ * zadeklarować tablicę numeryczną typu float, n elementowa,
+ * n - stała define
+ * Wczytać z klawiatury dane do tablicy (kwoty), w wersji 2 losowanie wynagrodzenia
+ * Na koniec należy wyliczyć kwotę sumaryzcną i średnią i ją wprowadzić.
+ *
+ * punkt rozwojowy:
+ *
+ *  Doróbka1:
+ *      Stworzenie prostego menuv wyboru:
+ *          - wczytanie kwoty
+ *          - listing danych, jaki on jest
+ *          - obliczenia: suma, średnia
+ *          - wyszukiwanie z przedziału z domkniętego przedziału kwot.
+ *              Podajemy widelki, min i max. Ma zwrócić znalezioną kwotę, podać jej index, ale
+ *              jeśli nie znajdzie ani jednego, to wyświetli komunikat, że takich kwot nie ma.
+ *          - Walidacja widełek - najpierw mniejsze, a potem większe. Mogą być równe
+ *          - Posortowanie, malejąco lub rosnąco.
+ */
+using namespace std;
+int main() {
+
+    return 0;
+}
