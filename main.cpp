150731c8f9c04e5ab195fd7130b1c51ce08c9304
corbatsh <bortlikkamil@gmail.com>
2022-12-19 21:32:59 +0100

    insert and show data functionalities added

--- main.cpp
+++ main.cpp
@@ -32,34 +32,38 @@ void badCinPrevent() {
 }
 
 void showMenu() {
-    cout << "Wybierz opcje z menu:" << endl;
-    cout << "[1] Wczytanie kwoty" << endl;
-    cout << "[2] Wyświetl dane" << endl;
-    cout << "[3] Obliczenia" << endl;
-    cout << "[4] Wyszukaj z wybranego przedziału" << endl;
-    cout << "[5] Zakończ" << endl;
-    cout << "Twój wybór: ";
+    cout << endl;
+    cout << "Pick an option from menu below:" << endl;
+    cout << "[1] Insert employee with salary" << endl;
+    cout << "[2] Show data" << endl;
+    cout << "[3] calculations" << endl;
+    cout << "[4] search for salary range" << endl;
+    cout << "[5] END" << endl;
+    cout << "Your choice: ";
 }
 
 int main() {
+    int memory = 10;
     int menuChoice;
     bool addMoreEmployees = true;
     string employeeName[10];
     float employeeSalary[10] = {0};
-    cout << "Aplikacja płacowa" << endl << endl;
+    float salariesSum = 0;
+    int employeesCount = 0;
+    cout << "Payroll Application" << endl << endl;
     do {
         showMenu();
         cin >> menuChoice;
         badCinPrevent();
         if (menuChoice == 5) return 0;
         while (menuChoice > 5 || menuChoice < 1) {
-            cout << endl << "Wartość nieprawidłowa! Wybierz jeszcze raz:" << endl << endl;
+            cout << endl << "Invalid Value! Pick again!:" << endl << endl;
             break;
         }
         switch (menuChoice) {
             case 1:
                 cout << "Your choice is to put new employee data: \n";
-                for (int i = 0; i < 10; ++i) {
+                for (int i = 0; i < memory; ++i) {
                     if (employeeSalary[i] == 0) {
                         cout << "Enter employee name: \n";
                         cin >> employeeName[i];
@@ -73,13 +77,41 @@ int main() {
                     }
                 }
             case 2:
-                cout << "work in progress1";
+                cout << endl;
+                for (int i = 0; i < memory; ++i) {
+                    if (employeeSalary[i] != 0) {
+                        cout << employeeName[i] << ": " << employeeSalary[i] << endl;
+                    }
+                }
                 break;
             case 3:
-                cout << "work in progress2";
+                cout << "Pick an option from menu below:" << endl;
+                cout << "[1] Sum salaries\n";
+                cout << "[2] calc average salary\n";
+                cout << "[3] Go back\n";
+                cin >> menuChoice;
+                badCinPrevent();
+                salariesSum = 0;
+                employeesCount = 0;
+                for (int i = 0; i < memory; ++i) {
+
+                    if (employeeSalary[i] != 0) {
+                        salariesSum += employeeSalary[i];
+                        employeesCount++;
+                    }
+
+                    if (menuChoice == 1) {
+                        cout << "Sum of employees salaries totals: " << salariesSum;
+                        break;
+                    } else if (menuChoice == 2) {
+                        cout << "Average salary totals: " << salariesSum / employeesCount;
+                        break;
+                    }
+                }
                 break;
             case 4:
                 cout << "work in progress3";
+                break;
             case 5:
                 break;
             default:
